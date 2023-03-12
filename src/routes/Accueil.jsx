21a980d73ae63e8b94c0db7dac055f75b3afffff
Thomas <thomas.basquin2@gmail.com>
2023-03-12 18:30:23 +0100

    Working on responsive mobile and desktop

--- src/routes/Accueil.jsx
+++ src/routes/Accueil.jsx
@@ -1,23 +1,28 @@
 import { Link } from "react-router-dom";
 
 function App() {
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
   return (
-    <div className="w-screen h-screen bg-pokemon-bg bg-center bg-no-repeat p-0 m-0 align-middle">
+    <div className="w-screen h-screen bg-pokemon-bg bg-center bg-no-repeat bg-cover p-0 m-0 align-middle">
       <img
         src="assets/Pokedex.png"
         alt="Pokedex"
-        className="w-9/12 mx-auto pt-28 md:w-64 md:pt-11 lg:w-[33rem] lg:pt-20 xl:w-[36rem] xl:pt-13 2xl:w-[39] "
+        className="w-9/12 mx-auto pt-2 md:w-64 md:pt-11 xl:w-80 xl:pt-10 2xl:w-[39] "
       />
-      <div className="w-full flex flex-col justify-center items-center absolute bottom-10 ">
+      <div className="w-full flex flex-col justify-center items-center absolute bottom-3 xl:bottom-7 ">
         <Link to="/pokedex">
           <img
             src="assets/pokeball.png"
             alt="pokeball"
-            className="w-24 bottom-12 animate-bounce"
+            className="w-24 bottom-12 animate-bounce xl:w-20 xl:bottom-6"
           />
         </Link>
         <Link to="/pokedex">
-          <h2 className="text-white mt-2 hover:text-gray-200">
+          <h2 className="text-white mt-2 hover:text-gray-200 xl:mt-2 xl:text-lg">
             Cliquer sur la Pokeball pour entrer
           </h2>
         </Link>
