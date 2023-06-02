e64e96572320cc44a3c3fde075851d4c073afbe3
Thomas <thomas.basquin2@gmail.com>
2023-06-02 20:52:07 +0200

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import "../tailwind.css";
 import "../public/assets/pokemonColors.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Pokedex</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
