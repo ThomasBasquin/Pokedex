5b175817b72b4909df06352a71111d6ce84a34d9
Thomas <thomas.basquin2@gmail.com>
2023-08-10 20:18:29 +0200

    remove bug of 800 pokemon only for swipe right

--- pages/pokedex.js
+++ pages/pokedex.js
@@ -41,7 +41,7 @@ const Pokedex = ({ initialPokemonData }) => {
   const handleSwipe = useSwipeable({
     onSwipedLeft: () =>
       setId((prevId) => {
-        if (prevId < 800) {
+        if (prevId < 899) {
           setDirection("right");
           preloadPokemonData(prevId + 2);
           return prevId + 1;
