7f13f6f630067f62d27c0f079244a93cc120b32f
Noe CARL <noe.carldev@gmail.com>
2023-09-12 21:54:07 +0200

    refactor: splited main content into a component

---
+++ components/PokemonDisplay.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import PokemonName from "../components/pokemonName";
+import PokemonInfo from "../components/pokemonInfo";
+import CapacityButton from "../components/capacityButton";
+import ImageGroup from "../components/imageGroup";
+import { useTransition, animated } from "react-spring";
+
+// todo: remove duplicates
+const capitalize = (type) => type.charAt(0).toUpperCase() + type.slice(1);
+
+const getTypeColors = (type) => {
+  const capType = capitalize(type);
+  return {
+    primaryClass: `primary${capType}`,
+    secondaryTextClass: `secondaryText${capType}`,
+    secondaryBackgroundClass: `secondaryBackground${capType}`,
+  };
+};
+
+function PokemonDisplay(props) {
+  const { direction, loading, id, primaryType } = props;
+
+  const { secondaryTextClass, secondaryBackgroundClass } =
+    getTypeColors(primaryType);
+
+  const transitions = useTransition(id, {
+    from: {
+      position: "absolute",
+      opacity: 0,
+      transform:
+        direction === "right"
+          ? "translate3d(100%,0,0)"
+          : "translate3d(-100%,0,0)",
+    },
+    enter: {
+      position: "absolute",
+      opacity: 1,
+      transform: "translate3d(0%,0,0)",
+    },
+    leave: {
+      position: "absolute",
+      opacity: 0,
+      transform:
+        direction === "right"
+          ? "translate3d(-50%,0,0)"
+          : "translate3d(50%,0,0)",
+    },
+    config: { duration: 300 },
+  });
+
+  return (
+    <>
+      {transitions((style, i) => (
+        <animated.div style={style}>
+          <ImageGroup id={i} color={secondaryTextClass} />
+          <div className="flex laptop-sm:hidden  flex-shrink justify-between mt-7">
+            <PokemonName id={id} />
+            {!loading && (
+              <CapacityButton color={secondaryBackgroundClass} id={id} />
+            )}
+          </div>
+          <PokemonInfo id={id} />
+        </animated.div>
+      ))}
+    </>
+  );
+}
+
+export default PokemonDisplay;
--- pages/pokedex.js
+++ pages/pokedex.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useMediaQuery } from "react-responsive";
-import PokemonName from "../components/pokemonName";
-import PokemonInfo from "../components/pokemonInfo";
-import CapacityButton from "../components/capacityButton";
-import ImageGroup from "../components/imageGroup";
+
 import { getPokemonData, preloadPokemonData } from "../services/pokemonApi";
 import { usePokemonData } from "../hooks/usePokemon";
 import classNames from "classnames";
 import PokemonListMobile from "../components/mobile/pokemonListMobile";
 import PokemonListDesktop from "../components/pc/pokemonListDesktop";
 import PokemonListVertical from "../components/pc/pokemonListVertical";
-import { useTransition, animated } from "react-spring";
 import { useSwipeable } from "react-swipeable";
 import Header from "../components/Header";
 import Loader from "../components/Loader";
+import PokemonDisplay from "../components/PokemonDisplay";
 
 const capitalize = (type) => type.charAt(0).toUpperCase() + type.slice(1);
 
@@ -65,7 +62,7 @@ const Pokedex = ({ initialPokemonData }) => {
     trackMouse: true,
   });
 
-  const { primaryClass, secondaryTextClass, secondaryBackgroundClass } =
+  const { primaryClass } =
     getTypeColors(primaryType);
   const dynamicPrimaryColorClass = classNames(
     "flex",
@@ -78,30 +75,7 @@ const Pokedex = ({ initialPokemonData }) => {
     "backgroundFade",
   );
 
-  const transitions = useTransition(id, {
-    from: {
-      position: "absolute",
-      opacity: 0,
-      transform:
-        direction === "right"
-          ? "translate3d(100%,0,0)"
-          : "translate3d(-100%,0,0)",
-    },
-    enter: {
-      position: "absolute",
-      opacity: 1,
-      transform: "translate3d(0%,0,0)",
-    },
-    leave: {
-      position: "absolute",
-      opacity: 0,
-      transform:
-        direction === "right"
-          ? "translate3d(-50%,0,0)"
-          : "translate3d(50%,0,0)",
-    },
-    config: { duration: 300 },
-  });
+
 
   return (
     <div className="h-full fixed flex justify-center">
@@ -117,18 +91,9 @@ const Pokedex = ({ initialPokemonData }) => {
         {loading && (
           <Loader />
         )}
-        {transitions((style, i) => (
-          <animated.div style={style}>
-            <ImageGroup id={i} color={secondaryTextClass} />
-            <div className="flex laptop-sm:hidden  flex-shrink justify-between mt-7">
-              <PokemonName id={id} />
-              {!loading && (
-                <CapacityButton color={secondaryBackgroundClass} id={id} />
-              )}
-            </div>
-            <PokemonInfo id={id} />
-          </animated.div>
-        ))}
+
+        <PokemonDisplay pokemon={data} direction={direction} loading={loading} id={id} primaryType={primaryType} />
+
       </div>
       {isMobile ? (
         <PokemonListMobile
